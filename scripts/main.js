9f04d531f915ab6971350a14f4e4ad9437fe9704
David van Leeuwen <david@madeawkward.com>
2013-08-13 12:22:50 +0200

    Add backbone.modal and backbone.marionette.modals

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,62 @@
 $(function() {
 	$('body').gistify();
+
+  // create a layout class
+  var Layout = Backbone.Marionette.Layout.extend({
+    template: _.template($('#modals-template').html()),
+    regions: {
+      modals: {
+        selector:   '.modals-container',
+        regionType: Backbone.Marionette.Modals
+      }  
+    }
+  });
+
+  // create a modal view class
+  var Modal1 = Backbone.Modal.extend({
+    template: _.template($('#modal-template1').html()),
+    viewContainer: '.my-container',
+    submitEl: '.bb-modal-button',
+    views: {
+      'click #tab1': {
+        name: 'tab1',
+        view: _.template($('#modal-tab1').html()),
+        onActive: 'setActive'
+      },
+      'click #tab2': {
+        name: 'tab2',
+        view: _.template($('#modal-tab2').html()),
+        onActive: 'setActive'
+      },
+      'click #tab3': {
+        name: 'tab3',
+        view: _.template($('#modal-tab3').html()),
+        onActive: 'setActive'
+      }
+    },
+    setActive: function(options) {
+      this.$('.tabbar-tab a').removeClass('active');
+      this.$('#'+options.name).addClass('active');
+    },
+    onClose: function() {
+      console.log('onClose');
+    }
+  });
+
+  var Modal2 = Backbone.Modal.extend({
+    template: _.template($('#modal-template2').html()),
+    submitEl: '.bb-modal-button'
+  });
+
+  var myLayout = new Layout();
+  $('body').append(myLayout.render().el);
+
+  $('body').on('click', '.open-1', function(){
+    myLayout.modals.show(new Modal1());
+  });
+
+  $('body').on('click', '.open-2', function(){
+    myLayout.modals.show(new Modal2());
+  });
+
 });
