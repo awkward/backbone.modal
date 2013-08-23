fc0f8e1965172eb91526d79798fc224805a28372
David van Leeuwen <david@madeawkward.com>
2013-08-23 11:19:04 +0200

    Working on stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 $(function() {
 	$('body').gistify();
 
-  // create a layout class
   var Layout = Backbone.Marionette.Layout.extend({
     template: _.template($('#modals-template').html()),
     regions: {
@@ -12,58 +11,90 @@ $(function() {
     }
   });
 
-  // create a modal view class
-  var Modal1 = Backbone.Modal.extend({
-    template: _.template($('#modal-template1').html()),
+  var TabModal = Backbone.Modal.extend({
+    template: _.template($('#modal-template').html()),
+
     viewContainer: '.my-container',
-    submitEl: '.bb-modal-button',
+
+    events: {
+      'click .open-info': 'openInfo'
+    },
+
     views: {
       'click #tab1': {
         name: 'tab1',
-        view: _.template($('#modal-tab1').html()),
+        view: _.template($('#modal-view1-template').html()),
         onActive: 'setActive'
       },
       'click #tab2': {
         name: 'tab2',
-        view: _.template($('#modal-tab2').html()),
-        onActive: 'setActive'
-      },
-      'click #tab3': {
-        name: 'tab3',
-        view: _.template($('#modal-tab3').html()),
+        view: _.template($('#modal-view2-template').html()),
         onActive: 'setActive'
       }
     },
-
     beforeCancel: function() {
-    	return false;
+      return false;
     },
-
     beforeSubmit: function() {
-    	return false;
+      return false;
     },
-
+    openInfo: function(e) {
+      e.preventDefault()
+      myLayout.modals.show(new InfoModal());
+    },
     setActive: function(options) {
       this.$('.tabbar-tab a').removeClass('active');
       this.$('#'+options.name).addClass('active');
-    },
-    onClose: function() {
-      console.log('onClose');
     }
   });
 
-  var Modal2 = Backbone.Modal.extend({
-    template: _.template($('#modal-template2').html()),
-    submitEl: '.bb-modal-button'
+  var InfoModal = Backbone.Modal.extend({
+    template: _.template($('#modal-template1').html()),
+    cancelEl: '.bb-modal-button',
+    events: {
+      'click .open-tour': 'openTour'
+    },
+    openTour: function(e) {
+      e.preventDefault()
+      myLayout.modals.show(new TourModal());
+    }
+  });
+
+  var TourModal = Backbone.Modal.extend({
+    submitEl: '.done',
+    cancelEl: '.cancel',
+
+    views: {
+      'click #step1': {
+        view: _.template($('#modal-step1-template').html())
+      },
+      'click #step2': {
+        view: _.template($('#modal-step2-template').html())
+      },
+      'click #step3': {
+        view: _.template($('#modal-step3-template').html())
+      }
+    },
+
+    events: {
+      'click .previous': 'previousStep',
+      'click .next': 'nextStep'
+    },
+
+    previousStep: function(e) {
+      e.preventDefault();
+      this.previous();
+    },
+
+    nextStep: function(e) {
+      e.preventDefault();
+      this.next();
+    }
+
   });
 
   var myLayout = new Layout();
   $('.app').append(myLayout.render().el);
 
-  myLayout.modals.show(new Modal1());
-
-  $('.app').on('click', '.open-2', function(){
-    myLayout.modals.show(new Modal2());
-  });
-
+  myLayout.modals.show(new TabModal());
 });
